c13d99689d6c479c5df6db17e8f437dd4cfd7f52
Ian Rosas <ian@therocketcode.com>
2021-08-24 20:13:45 -0500

    primer ejercicio resuelto

--- semana1/challenge1.js
+++ semana1/challenge1.js
@@ -2,7 +2,7 @@ const reverseString = (str) => {
     //Implementación
     if(typeof(str) !== "string" || str.length <= 1 || str.length > 15) 
         throw new Error("Tipo de dato o longitud no admitidos");
-    
+        
     newText = str.split("");
     reverse = newText.reverse();
     return reverse.join("");
